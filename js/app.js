55de3b29538fdcb4375a301a70ed108794e83368
mervydevmedia <65782957+kgofweb@users.noreply.github.com>
2022-07-17 15:09:52 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,6 +2,19 @@
 const modalBox = document.querySelector('.modal')
 const codeCountry = document.getElementById('code__country')
 const labelChecked = document.querySelectorAll('.modal-body input')
+const textAnim = document.querySelector('h2')
+
+
+new Typewriter(textAnim, {
+  // Speed delete
+  deleteSpeed: 20
+})
+.typeString('Bienvenue sur Chap<span style="color: #ff4444;">Money</span>')
+.pauseFor(1000)
+.deleteChars(23)
+.pauseFor(800)
+.typeString('Bienvenue sur Chap<span style="color: #ff4444;">Money</span>')
+.start()
 
 // Event Listener
 labelChecked.forEach(el => {
@@ -24,7 +37,7 @@ const sr = ScrollReveal({
 
 sr.reveal(`.logo`, { origin: 'top', delay: 300 })
 sr.reveal(`.title`, { origin: 'bottom', delay: 500 })
-sr.reveal(`.link__1`, { origin: 'left', delay: 600 })
+sr.reveal(`.link__1`, { origin: 'top', delay: 600 })
+sr.reveal(`.link__3`, { origin: 'top', delay: 600 })
 sr.reveal(`.link__2`, { origin: 'bottom', delay: 700 })
-sr.reveal(`.link__3`, { origin: 'right', delay: 800 })
-sr.reveal(`.link__4`, { origin: 'bottom', delay: 900 })
+sr.reveal(`.link__4`, { origin: 'bottom', delay: 700 })
